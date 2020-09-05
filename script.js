a6d0d5964d41ba58f20a4028f55d693ff4a9000f
JHardacker <jhardacker@gmail.com>
2020-09-05 10:29:53 -0400

    Adding homework folder

---
+++ script.js
@@ -0,0 +1,59 @@
+// Variables
+var saveBtn = $(".saveBtn");
+var currentHour = moment().format("HH"); //current hour 
+var currentHourInt = parseInt(currentHour); 
+
+$("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("#10Row").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
+$("#11Row").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
+$("#12Row").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
+$("#1Row").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
+$(document).ready(function () {
+
+    // Function to keep data 
+    renderPlans();
+
+  // Current Date and Time 
+  $("#currentDay").append();
+
+  function addDate() { 
+  $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
+  
+  } setInterval(addDate, 1000);
+
+  for (var i = 0; i <= 12; i++) {  
+
+      var inputHour = $("#" + i + "Row").attr("data-time"); 
+      var inputHourInt = parseInt(inputHour); 
+
+      if (currentHourInt === inputHourInt) {
+          $("#" + i + "Row").addClass("present"); // Red for Present Hour
+      }
+      if (currentHourInt > inputHourInt) { // Gray for Past Hour
+          $("#" + i + "Row").addClass("past");
+      }
+      if (currentHourInt < inputHourInt) { // Green for Future Hour 
+          $("#" + i + "Row").addClass("future");
+      }
+    }
+
+    // Save Button
+    saveBtn.on("click", function () { 
+
+      var rowHour = $(this).attr("data-hour"); 
+      var input = $("#" + rowHour + "Row").val(); 
+      localStorage.setItem(rowHour, input); 
+    });
+  
+    //  Get stored input 
+    function renderPlans() {
+      for (var i = 0; i <= 12; i++) {
+      $("#" + i + "Row").val(localStorage.getItem(i));
+      }
+    }
+});
